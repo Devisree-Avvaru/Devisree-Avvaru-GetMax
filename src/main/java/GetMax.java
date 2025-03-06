784f8b0363f95fa409f7d68163633069c0584a1a
Devisree Avvaru <devisree.avvarus@gmail.com>
2025-03-06 21:55:45 +0000

    Completed Lab Get Max

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,14 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int lar=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>lar){
+                lar=arr[i];
+                }
+            }
+        return lar;
+        
+
     }
 }
